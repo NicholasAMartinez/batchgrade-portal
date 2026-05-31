c04c01364e615ea130fa8b08c003f53d43fccfb3
Nicholas <nicholasammartinez@gmail.com>
2026-05-31 14:43:37 -0700

    feat: Improved UI

--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -1,34 +1,101 @@
 /**
- * Basic CRUD user interface. This will handle 
+ * Basic CRUD user interface.
  */
 
+"use client";
+
+import { useState } from "react";
+
 export function DashboardContent() {
+  const [editing, setEditing] = useState(false);
+
   return (
-    <div className="w-full max-w-7xl p-4 border ">
-      <div className="flex mb-4 p-3 justify-between border">
-        <div>Test Cases</div>
-        <button className="border  px-3">
+    <div className="w-full max-w-7xl border">
+      <div className="border-b p-4">
+        <div className="font-semibold">Test Cases</div>
+      </div>
+
+      <div className="grid grid-cols-4 gap-2 border-b p-4">
+        <input
+          className="border bg-transparent p-2"
+          placeholder="Name"
+        />
+        <input
+          className="border bg-transparent p-2"
+          placeholder="Input"
+        />
+        <input
+          className="border bg-transparent p-2"
+          placeholder="Output"
+        />
+        <button
+          className="inline-flex h-10 w-28 items-center justify-center justify-self-end rounded-md border border-[var(--ring)] bg-primary px-4 py-2 text-sm font-medium text-[var(--ring)] hover:bg-white/10"
+          type="button"
+        >
           + Add Case
         </button>
       </div>
 
-      <div className="border ">
-        <div className="flex justify-between border-b  p-3">
-          <div className="border  p-2">
-            ID | USER_ID | INSERTED_AT | UPDATED_AT | NAME | INPUT | OUTPUT 
+      <div>
+        <div className="border-b p-4">
+          <div className="grid grid-cols-4 gap-2">
+            <div>
+              <div className="text-sm">Name</div>
+              <div>Test Case</div>
+            </div>
+            <div>
+              <div className="text-sm">Input</div>
+              <div>Nicholas</div>
+            </div>
+            <div>
+              <div className="text-sm">Output</div>
+              <div>Hello Nicholas!</div>
+            </div>
+
+            <div className="flex justify-self-end gap-2">
+              <button
+                className="inline-flex h-10 w-20 items-center justify-center rounded-md border bg-background px-3 text-sm font-medium hover:bg-white/10"
+                onClick={() => setEditing(!editing)}
+                type="button"
+              >
+                Edit
+              </button>
+
+              <button
+                className="inline-flex h-10 w-20 items-center justify-center rounded-md border px-3 text-sm font-medium text-red-500 hover:bg-red-700/50"
+                type="button"
+              >
+                Delete
+              </button>
+            </div>
           </div>
 
-          <div className="flex gap-2 border  p-2">
-            <button className="border  px-2">
-              ...
-            </button>
-            <button className="border  px-2 text-red-600">
-              X
-            </button>
-          </div>
+          {/* Editing option if active */}
+          {editing && (
+            <div className="mt-3 grid grid-cols-4 gap-2 border-t p-3">
+              <input
+                className="border bg-transparent p-2"
+                placeholder="Name"
+              />
+              <input
+                className="border bg-transparent p-2"
+                placeholder="Input"
+              />
+              <input
+                className="border bg-transparent p-2"
+                placeholder="Output"
+              />
+              <button
+                className="inline-flex h-10 w-20 items-center justify-center justify-self-end rounded-md border border-[var(--ring)] bg-primary px-4 py-2 text-sm font-medium text-[var(--ring)] hover:bg-white/10"
+                type="button"
+              >
+                Save
+              </button>
+            </div>
+          )}
         </div>
 
-        <div className="flex justify-center p-3">
+        <div className="flex justify-center p-4 text-[var(--muted-foreground)]">
           No Test Cases Found
         </div>
       </div>
