3504a927cc03739bb736d676292eaed68de2f5b8
Nicholas <nicholasammartinez@gmail.com>
2026-05-31 13:48:30 -0700

    feat: Basic Layout Added

--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -1,13 +1,37 @@
-
-
+/**
+ * Basic CRUD user interface. This will handle 
+ */
 
 export function DashboardContent() {
   return (
-    <div>
+    <div className="w-full max-w-7xl p-4 border ">
+      <div className="flex mb-4 p-3 justify-between border">
+        <div>Test Cases</div>
+        <button className="border  px-3">
+          + Add Case
+        </button>
+      </div>
 
-      Placeholder For the DashboardContent
+      <div className="border ">
+        <div className="flex justify-between border-b  p-3">
+          <div className="border  p-2">
+            ID | USER_ID | INSERTED_AT | UPDATED_AT | NAME | INPUT | OUTPUT 
+          </div>
 
+          <div className="flex gap-2 border  p-2">
+            <button className="border  px-2">
+              ...
+            </button>
+            <button className="border  px-2 text-red-600">
+              X
+            </button>
+          </div>
+        </div>
+
+        <div className="flex justify-center p-3">
+          No Test Cases Found
+        </div>
+      </div>
     </div>
-
   );
 }
